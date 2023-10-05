b15a0840e22d5fbce87f2e046ca4ac2b10640528
hoang <hoangthai240911@gmal.com>
2023-10-05 15:16:18 +0700

    Sửa lại tên file

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 
-class Dashboard extends Controller
+class DashboardController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return view('dashboard.index');
+        return view('admin/dashboard/index');
     }
 
     /**
